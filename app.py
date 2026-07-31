98d9c56c4ef299b70a37e5bae476baf0a70ca137
lukyuvraj-lab <lukyuvraj@gmail.com>
2026-08-01 01:50:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -295,21 +295,6 @@ st.dataframe(
 )
 
 # =====================================================
-# BAR CHART
-# =====================================================
-st.markdown("---")
-st.subheader("📈 Plant-wise Pending Value Chart")
-
-chart_data = (
-    plant_summary[
-        plant_summary["Plant"] != "TOTAL"
-    ]
-    .set_index("Plant")
-)
-
-st.bar_chart(chart_data["Value"])
-
-# =====================================================
 # PIE CHART
 # =====================================================
 st.markdown("---")

22f8639c9934160dc67573c9afabc99dfcd8d740
lukyuvraj-lab <lukyuvraj@gmail.com>
2026-08-01 20:04:29 +0530

    Update date display styling in app.py
    Adjust margin and font size for date display.

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ with col1:
 with col2:
     st.markdown(
         f"""
-        <div style="margin-top:70px; text-align:right; font-size:25px;">
+        <div style="margin-top:60px; text-align:right; font-size:15px;">
             📅 {datetime.now().strftime('%d-%m-%Y')}
         </div>
         """,

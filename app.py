e98c2d2dbd651999b78d46a6ac0547727876a7e0
lukyuvraj-lab <lukyuvraj@gmail.com>
2026-08-04 11:47:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -239,24 +239,7 @@ df["Department"] = df["Material"].apply(
 # -----------------------------
 # Department Logic
 # -----------------------------
-def get_department(material):
 
-    material = str(material)
-
-    if (
-        material.startswith("1000")
-        or material.startswith("385")
-        or material.startswith("44")
-        or material.startswith("45")
-        or material.startswith("46")
-        or material.startswith("485")
-        or material.startswith("63")
-    ):
-        return "Electrical"
-
-    return "Mechanical"
-
-df["Department"] = df["Material"].apply(get_department)
 
 # Convert Plant codes to names
 plant_map = {
